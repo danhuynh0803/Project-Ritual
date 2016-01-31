5baf17f766fada5b091fc1cd52c95f1427f8f1ad
B. Adam Russell <b.ad.russell@gmail.com>
2016-01-31 10:21:47 -0500

    simple grid system

---
+++ Assets/Tile.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Tile : MonoBehaviour {
+
+	public GameObject backgroundGO;
+	public GameObject buildingGO;
+
+	public Sprite[] buildings;
+
+	public int column;
+	public int row;
+	public TileKinds tileKind;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Init(float length, int x, int y, TileKinds initialTileKind, GameObject parentGO) {
+		column = x;
+		row = y;
+		tileKind = initialTileKind;
+
+		this.gameObject.transform.SetParent(parentGO.transform);
+		this.SetSize(length);
+		Vector2 screenScale = this.GetComponent<RectTransform>().localScale;
+
+		this.gameObject.transform.localPosition = new Vector3(x+0.5f, -y-0.5f, 1f) * length * screenScale.x;
+		this.SetTileKind();
+	}
+
+	public void SetSize(float length) {
+		RectTransform rectTransform = this.GetComponent<RectTransform>();
+		rectTransform.sizeDelta = new Vector2(length, length);
+	}
+
+	Color RandomGreenColor() {
+		return new Color(Random.Range(0f, 0.5f), Random.Range(0.5f, 0.8f), Random.Range(0f, 0.4f));
+	}
+
+	void SetTileKind() {
+		switch (tileKind) {
+			case TileKinds.Castle:
+				backgroundGO.GetComponent<Image>().color = this.RandomGreenColor();
+				buildingGO.GetComponent<Image>().sprite = buildings[0];
+				buildingGO.SetActive(true);
+				break;
+			case TileKinds.Hideout:
+				backgroundGO.GetComponent<Image>().color = this.RandomGreenColor();
+				buildingGO.GetComponent<Image>().sprite = buildings[1];
+				buildingGO.SetActive(true);
+				break;
+			default:
+				backgroundGO.GetComponent<Image>().color = this.RandomGreenColor();
+				buildingGO.GetComponent<Image>().sprite = null;
+				buildingGO.SetActive(false);
+				break;
+		}
+	}
+
+	public void ClickTile() {
+		Debug.Log(column + "," + row + ": " + tileKind);
+	}
+}
---
+++ Assets/TileGridController.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public enum TileKinds {
+	Empty=0,
+	Castle=1,
+	Hideout=2,
+}
+
+public enum LevelKinds {
+	Level1=1,
+	Level2=2,
+	Level3=3,
+	Level4=4,
+}
+
+public class TileGridController : MonoBehaviour {
+
+	public GameObject tilePrefab;
+	public Tile[,] tiles;
+	public GameObject tileGridParentGO;
+	public LevelKinds currentLevel;
+
+	// Use this for initialization
+	void Start () {
+		this.LoadMap(currentLevel);
+	}
+
+	public TileKinds GetTileKind(string tileCode) {
+		switch (tileCode) {
+			case "c":
+				return TileKinds.Castle;
+			case "h":
+				return TileKinds.Hideout;
+			default:
+				return TileKinds.Empty;
+		}
+	}
+
+	Tile[,] LoadMap(LevelKinds levelKind) {
+		string[] mapData;
+		switch (levelKind) {
+			case LevelKinds.Level1:
+				mapData = new string[2];
+				mapData[0] = "x,x";
+				mapData[1] = "x,c";
+				break;
+			case LevelKinds.Level2:
+				mapData = new string[3];
+				mapData[0] = "x,x,x";
+				mapData[1] = "x,c,x";
+				mapData[2] = "x,x,x";
+				break;
+			case LevelKinds.Level3:
+				mapData = new string[4];
+				mapData[0] = "x,x,x,x";
+				mapData[1] = "x,c,x,x";
+				mapData[2] = "x,x,x,x";
+				mapData[3] = "x,x,x,x";
+				break;
+			case LevelKinds.Level4:
+				mapData = new string[5];
+				mapData[0] = "x,x,x,x,x";
+				mapData[1] = "x,c,x,x,x";
+				mapData[2] = "x,x,x,x,x";
+				mapData[3] = "x,h,x,c,x";
+				mapData[4] = "x,x,x,x,x";
+				break;
+			default:
+				mapData = new string[1];
+				mapData[0] = "x";
+				break;
+		}
+
+		int rows = mapData.Length;
+		int columns = mapData[0].Split(',').Length;
+		float tileLength = Screen.height / rows;
+		Tile[,] tiles = new Tile[columns, rows];
+
+		for (int y=0; y<rows; y++) {
+			string rowString = mapData[y];
+			string[] columnCodes = rowString.Split(',');
+			for (int x=0; x<columnCodes.Length; x++) {
+				TileKinds tileKind = this.GetTileKind(columnCodes[x]);
+				GameObject newTileGO = Instantiate(tilePrefab, Vector3.zero, Quaternion.identity) as GameObject;
+				Tile tile = newTileGO.GetComponent<Tile>();
+				tile.Init(tileLength, x, y, tileKind, tileGridParentGO);
+				tiles[x, y] = tile;
+			}
+		}
+
+		return tiles;
+
+	}
+}
